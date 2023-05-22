d074a1b8bfdee8fadaaac2729a0929ee58a6614b
Nisarat-A <noansrnn@gmail.com>
2023-05-22 14:13:30 +0700

    -

--- src/JavaIO/InputStreamExample.java
+++ src/JavaIO/InputStreamExample.java
@@ -1,4 +1,17 @@
 package JavaIO;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 public class InputStreamExample {
+    public static void main(String[] args) throws IOException {
+        InputStream inputStream = null ;
+        try{
+            inputStream = new FileInputStream("text.txt");
+            byte[] bytes = new byte[1024] ;
+            int index;
+            while((index = inputStream.read(bytes)) != -1)
+            {System.out.println("index : " + index);}}
+            finally{if (inputStream != null) { inputStream.close(); }}}
 }
--- src/JavaIO/OutputStreamExample.java
+++ src/JavaIO/OutputStreamExample.java
@@ -1,2 +1,28 @@
-package JavaIO;public class OutputStreamExample {
+package JavaIO;
+
+import java.io.*;
+
+public class OutputStreamExample {
+    public static void main(String[] args) throws IOException {
+        InputStream inputStream = null;
+        OutputStream outputStream = null;
+        try {
+            inputStream = new FileInputStream("text.txt");
+            outputStream = new FileOutputStream("text.txt");
+            byte[] bytes = new byte[1024];
+            int index;
+            while ((index = inputStream.read(bytes)) != -1) {
+                outputStream.write(bytes, 0, index);
+            }
+
+        } finally {
+            if(outputStream != null){
+                outputStream.close();
+            }
+
+            if (inputStream != null) {
+                inputStream.close();
+            }
+        }
+    }
 }
